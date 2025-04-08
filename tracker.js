24505157a9f32e0e39f5d1182d91cb743fecfcd2
khatriasim <khatriasim111@gmail.com>
2025-04-08 16:19:53 +0545

    Initial commit

---
+++ tracker.js
@@ -0,0 +1,8 @@
+let category = document.getElementById("category")
+let options = ["food", "veggies", "snacks"]
+
+category.forEach(option => {
+    let option = document.createElement("option")
+    option = appendChild(category)
+    
+});
